ca8d33f3f8f081d4b8b222a84c14f92145dece65
AlexAlvarez <44286902+AlexAlvarezAlmendros@users.noreply.github.com>
2024-08-14 15:06:30 +0200

    MinorFixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,18 @@ import Inventory from './components/Inventory';
 
 function App() {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [selectedObject, setSelectedObject] = useState(null);
-  const [objects, setObjects] = useState([]); // Definición de objects y setObjects
+  const selectedObjectRef = useRef(null); // Usar useRef para el objeto seleccionado
+  const [objects, setObjects] = useState([]);
   const sceneRef = useRef();
   const rendererRef = useRef();
   const cameraRef = useRef();
   const composerRef = useRef();
   const outlinePassRef = useRef();
   const isDragging = useRef(false);
-  const isSpacePressed = useRef(false); // Cambiar a useRef para evitar re-renderizados
+  const isSpacePressed = useRef(false);
   const previousMousePosition = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
-    // Crear la escena una sola vez
     const scene = new THREE.Scene();
     sceneRef.current = scene;
 
@@ -35,47 +34,41 @@ function App() {
       1000
     );
 
-    // Posicionar la cámara para una vista isométrica
-    camera.position.set(10, 10, 10); // Mover la cámara en los tres ejes
-    camera.zoom = 1; // Puedes ajustar el zoom para acercar/alejar
-    camera.lookAt(0, 0, 0); // Hacer que la cámara mire al origen
+    camera.position.set(10, 10, 10);
+    camera.zoom = 1;
+    camera.lookAt(0, 0, 0);
     cameraRef.current = camera;
     camera.updateProjectionMatrix();
 
-    // Crear el renderizador una sola vez
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('canvas-container').appendChild(renderer.domElement);
     rendererRef.current = renderer;
 
-    // Añadir la rejilla a la escena
     const gridHelper = new THREE.GridHelper(19, 19);
     scene.add(gridHelper);
 
-    // Post-processing para resaltar objetos seleccionados
     const composer = new EffectComposer(renderer);
     composer.addPass(new RenderPass(scene, camera));
 
     const outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, camera);
-    outlinePass.edgeStrength = 3; // Intensidad del contorno
-    outlinePass.edgeGlow = 0.5; // Resplandor del contorno
-    outlinePass.edgeThickness = 1.0; // Grosor del contorno
-    outlinePass.visibleEdgeColor.set('#d62b94'); // Color del contorno
-    outlinePass.hiddenEdgeColor.set('#d62b94'); // Color del contorno en los bordes ocultos
+    outlinePass.edgeStrength = 3;
+    outlinePass.edgeGlow = 0.5;
+    outlinePass.edgeThickness = 1.0;
+    outlinePass.visibleEdgeColor.set('#d62b94');
+    outlinePass.hiddenEdgeColor.set('#d62b94');
     composer.addPass(outlinePass);
     composerRef.current = composer;
     outlinePassRef.current = outlinePass;
 
-    // Iniciar la animación
     function animate() {
       requestAnimationFrame(animate);
       composer.render();
     }
     animate();
 
-    // Cleanup en desmontaje
     return () => {
-      renderer.domElement.remove(); // Elimina el canvas al desmontar el componente
+      renderer.domElement.remove();
     };
   }, []);
 
@@ -96,17 +89,17 @@ function App() {
     switch (type) {
       case 'truss':
         geometry = new THREE.BoxGeometry(1, 1, 4);
-        offset = { x: 0.5, y: 0.5, z: 2 }; // Centrar el truss en la rejilla
+        offset = { x: 0.5, y: 0.5, z: 2 };
         material = new THREE.MeshBasicMaterial({ color: 0xe08626 });
         break;
       case 'light':
         geometry = new THREE.SphereGeometry(0.5, 32, 32);
-        offset = { x: 0.5, y: 0.5, z: 2 }; // Centrar la luz en la rejilla
+        offset = { x: 0.5, y: 0.5, z: 2 };
         material = new THREE.MeshBasicMaterial({ color: 0x2b75d6 });
         break;
       case 'speaker':
         geometry = new THREE.CylinderGeometry(0.5, 0.5, 2, 32);
-        offset = { x: 0.5, y: 0.5, z: 1 }; // Centrar el altavoz en la rejilla
+        offset = { x: 0.5, y: 0.5, z: 1 };
         material = new THREE.MeshBasicMaterial({ color: 0xd62b94 });
         break;
       default:
@@ -117,14 +110,14 @@ function App() {
     newObject.position.set(position.x, position.y, position.z);
     scene.add(newObject);
 
-    setObjects((prevObjects) => [...prevObjects, newObject]); // Actualizar el estado correctamente
-
-    // Hacer que el objeto sea seleccionable
+    setObjects((prevObjects) => [...prevObjects, newObject]);
     newObject.userData.isSelectable = true;
-    // Configurar DragControls
+
     const dragControls = new DragControls([newObject], cameraRef.current, rendererRef.current.domElement);
     dragControls.addEventListener('dragstart', function (event) {
       event.object.material.opacity = 0.5;
+      selectedObjectRef.current = event.object; // Asegura que el objeto seleccionado sea el que se está arrastrando
+      outlinePassRef.current.selectedObjects = [event.object]; // Aplicar contorno solo al objeto arrastrado
     });
     dragControls.addEventListener('dragend', function (event) {
       event.object.material.opacity = 1.0;
@@ -135,8 +128,6 @@ function App() {
 
   const handleSelect = (itemType) => {
     setSelectedItem(itemType);
-
-    // Añadir el objeto seleccionado a la escena en una posición predeterminada
     const defaultPosition = moveToGrid({ x: 1, y: 1, z: 0.5 });
     addObjectToScene(itemType, defaultPosition);
   };
@@ -145,37 +136,42 @@ function App() {
     const mouse = new THREE.Vector2();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
+  
     const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse, cameraRef.current);
-    const intersects = raycaster.intersectObjects(objects);
-
+  
+    // Obtener las intersecciones ordenadas por distancia (normalmente ya están ordenadas)
+    const intersects = raycaster.intersectObjects(objects, true);  // true asegura que también se consideren los hijos de objetos
+  
     if (intersects.length > 0) {
-      const object = intersects[0].object;
-
-      // Si hay un objeto previamente seleccionado, quita su contorno
-      if (selectedObject) {
-        outlinePassRef.current.selectedObjects = [];
+      // El primer objeto es el más cercano a la cámara
+      const closestObject = intersects[0].object;
+  
+      // Solo actualizar si el objeto seleccionado es diferente
+      if (selectedObjectRef.current !== closestObject) {
+        if (selectedObjectRef.current) {
+          outlinePassRef.current.selectedObjects = [];
+        }
+  
+        selectedObjectRef.current = closestObject;
+        outlinePassRef.current.selectedObjects = [closestObject];
+  
+        console.log("Selected Object:", closestObject);
       }
-
-      // Selecciona el nuevo objeto y añade el contorno
-      setSelectedObject(object);
-      outlinePassRef.current.selectedObjects = [object];
-
-      console.log("Selected Object:", object);
     }
   };
+  
+  
 
   const handleMouseDown = (event) => {
-    if (isSpacePressed.current && !selectedObject) {
+    if (isSpacePressed.current && !selectedObjectRef.current) {
       isDragging.current = true;
       previousMousePosition.current = {
         x: event.clientX,
         y: event.clientY,
       };
 
-      // Deseleccionar todos los objetos
-      setSelectedObject(null);
+      selectedObjectRef.current = null;
       outlinePassRef.current.selectedObjects = [];
     }
   };
@@ -188,7 +184,6 @@ function App() {
       const rotationSpeed = 0.005;
       const camera = cameraRef.current;
 
-      // Rotar la cámara alrededor del origen
       camera.position.x = camera.position.x * Math.cos(deltaX * rotationSpeed) + camera.position.z * Math.sin(deltaX * rotationSpeed);
       camera.position.z = camera.position.z * Math.cos(deltaX * rotationSpeed) - camera.position.x * Math.sin(deltaX * rotationSpeed);
       camera.position.y += deltaY * rotationSpeed;
@@ -210,34 +205,30 @@ function App() {
       isSpacePressed.current = true;
     }
   
+    const selectedObject = selectedObjectRef.current;
+  
     if (!selectedObject) {
       console.log("No object selected");
       return;
     }
-  
-    console.log("Selected Object:", selectedObject); // Verificar que el objeto esté correctamente seleccionado
   
     let moved = false;
   
     switch (event.key) {
       case 'ArrowUp':
         selectedObject.position.z -= 0.5;
-        console.log("ArrowUp", selectedObject.position);
         moved = true;
         break;
       case 'ArrowDown':
         selectedObject.position.z += 0.5;
-        console.log("ArrowDown", selectedObject.position);
         moved = true;
         break;
       case 'ArrowLeft':
         selectedObject.position.x -= 0.5;
-        console.log("ArrowLeft", selectedObject.position);
         moved = true;
         break;
       case 'ArrowRight':
         selectedObject.position.x += 0.5;
-        console.log("ArrowRight", selectedObject.position);
         moved = true;
         break;
       default:
@@ -247,28 +238,36 @@ function App() {
     if (moved) {
       const newPosition = moveToGrid(selectedObject.position, 0.5);
       selectedObject.position.set(newPosition.x, newPosition.y, newPosition.z);
-  
-      // Forzar la actualización de la escena si es necesario
+
       if (rendererRef.current && sceneRef.current && cameraRef.current) {
         rendererRef.current.render(sceneRef.current, cameraRef.current);
       }
     }
   };
-  
+
   const handleMouseWheel = (event) => {
+    const selectedObject = selectedObjectRef.current;
+  
     if (!selectedObject) return;
-    console.log("mousewheel");
-    // Controlar la velocidad de rotación
-    const rotationSpeed = 0.05;
-
-    // Rotar en el eje Y al girar la rueda del mouse
-    selectedObject.rotation.y += event.deltaY * rotationSpeed;
-
-    // Forzar la actualización de la escena si es necesario
+  
+    // Definir la rotación en incrementos de 45 grados (π/4 radianes)
+    const rotationIncrement = Math.PI / 4;
+  
+    // Obtener la dirección de la rueda del mouse (positivo o negativo)
+    const direction = event.deltaY > 0 ? 1 : -1;
+  
+    // Calcular la nueva rotación ajustada a 45 grados
+    const newRotation = selectedObject.rotation.y + direction * rotationIncrement;
+  
+    // Redondear la rotación al múltiplo más cercano de 45 grados
+    selectedObject.rotation.y = Math.round(newRotation / rotationIncrement) * rotationIncrement;
+  
+    // Forzar la actualización de la escena
     if (rendererRef.current && sceneRef.current && cameraRef.current) {
       rendererRef.current.render(sceneRef.current, cameraRef.current);
     }
   };
+  
 
   const handleKeyUp = (event) => {
     if (event.code === 'Space') {
@@ -283,7 +282,7 @@ function App() {
     window.addEventListener('mousedown', handleMouseDown);
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('mouseup', handleMouseUp);
-    window.addEventListener('wheel', handleMouseWheel); // Agregar evento para la rueda del mouse
+    window.addEventListener('wheel', handleMouseWheel);
 
     return () => {
       window.removeEventListener('click', handleMouseClick);
@@ -292,16 +291,14 @@ function App() {
       window.removeEventListener('mousedown', handleMouseDown);
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('mouseup', handleMouseUp);
-      window.removeEventListener('wheel', handleMouseWheel); // Remover evento para la rueda del mouse
+      window.removeEventListener('wheel', handleMouseWheel);
     };
   }, [objects]);
 
   return (
     <div className="App">
-      <h1>3D Event Design Tool</h1>
-      <div className="inventory">
+      <h1>Venue Planner 3D</h1>
         <Inventory onSelect={handleSelect} />
-      </div>
       <div id="canvas-container" />
     </div>
   );
